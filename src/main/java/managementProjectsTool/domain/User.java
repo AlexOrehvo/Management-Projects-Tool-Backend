15fc8ff944ed6aaf213158c0fc56e5121b83090d
a.orehvo <a.orehvo>
2019-11-18 12:23:09 -0500

    1

--- src/main/java/managementProjectsTool/domain/User.java
+++ src/main/java/managementProjectsTool/domain/User.java
@@ -3,8 +3,13 @@ package managementProjectsTool.domain;
 import managementProjectsTool.common.enums.Permission;
 import managementProjectsTool.domain.common.UUIDEntity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.validation.Constraint;
+import java.sql.Date;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -16,6 +21,22 @@ public class User extends UUIDEntity {
     @Column(name = "last_name")
     private String lastName;
 
+    @Column(name = "email", unique = true)
+    private String email;
 
-    private Set<Permission>
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "last_login")
+    private Date last_login;
+
+    @Column(name = "last_logout")
+    private Date last_logout;
+
+    @Column(name = "email_authorized")
+    private Boolean emailAuthorized = false;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private Set<Permission> permissions = new HashSet<>();
 }
+
